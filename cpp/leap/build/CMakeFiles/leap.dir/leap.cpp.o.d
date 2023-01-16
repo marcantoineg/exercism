9656e66c5a2a281fd387a9b8f851d56afde2d06a
Marc-Antoine <magiguere@mirego.com>
2023-01-16 12:14:24 -0500

    apply cpp's .gitignore

--- cpp/leap/build/CMakeFiles/leap.dir/leap.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/leap.dir/leap.cpp.o: \
-  /Users/marc-antoinegiguere/dev/studies/exercism/cpp/leap/leap.cpp \
-  /Users/marc-antoinegiguere/dev/studies/exercism/cpp/leap/leap.h
--- cpp/space-age/build/CMakeFiles/space-age.dir/space_age.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/space-age.dir/space_age.cpp.o: \
-  /Users/marc-antoinegiguere/dev/studies/exercism/cpp/space-age/space_age.cpp \
-  /Users/marc-antoinegiguere/dev/studies/exercism/cpp/space-age/space_age.h
